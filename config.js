ca433fd335f5f68dcca88973ccd8d6b5e8d534b3
Devie09 <90981737+Devie09@users.noreply.github.com>
2021-09-19 14:36:40 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const chrToken = process.env['token']
+/* Your Bot Default Prefix For Every Server */
+exports.Default_Prefix = "-";
+/* Your Bot All Embeds Color */
+exports.Color = "Green";
+/* Your Bot Token (Secret) */
+exports.Token = chrToken;
+/* Bot's Owner ID */
+exports.OwnerID = "DevinDoodleDoo#2242";
